c0927e7146c7b4dbeb8058e351657c011d5f4228
Hiroshi Miura <hirmiura@gmail.com>
2022-01-19 09:45:20 +0900

    test(bmfc): loadのテストを追加した

--- test/test_BMFC.py
+++ test/test_BMFC.py
@@ -11,6 +11,9 @@ sys.path.append(os.path.abspath('./src'))
 from BMFC import BMFC  # noqa: E402
 # fmt:on
 
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
 
 def test_applyDict():
     d = {
@@ -29,3 +32,29 @@ def test_applyDict():
     assert t.chars[1].begin == 13
     assert t.chars[1].end == 21
     assert t.chars[2].begin == 33
+
+
+def test_load():
+    f = os.path.join(__location__, 'test.bmfc')
+    t = BMFC.load(f)
+    assert t is not None
+    assert t.fileVersion == 1
+    assert t.fontName == 'TamaTou'
+    assert t.fontFile == 'TamaTou-Regular.otf'
+    assert t.charSet == 0
+    assert t.fontSize == 20
+    assert t.aa == 4
+    assert t.scaleH == 100
+    assert t.useSmoothing == 1
+    assert t.isBold == 0
+    assert t.isItalic == 0
+    assert t.useUnicode == 1
+    assert t.disableBoxChars == 1
+    assert t.outputInvalidCharGlyph == 0
+    assert t.dontIncludeKerningPairs == 0
+    assert t.useHinting == 1
+    assert t.renderFromOutline == 0
+    assert t.useClearType == 1
+    assert t.autoFitNumPages == 0
+    assert t.autoFitFontSizeMin == 0
+    assert t.autoFitFontSizeMax == 0
